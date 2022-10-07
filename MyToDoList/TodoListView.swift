df3cd62d72ed11c103a425301a5ef589e09de6d2
JJuKyungs <wnrud675@naver.com>
2022-10-07 14:39:11 +0900

    [chore] MainView 추가 사항 예정 및 List 수정 및 추가사항 주석으로 작성

--- MyToDoList/TodoListView.swift
+++ MyToDoList/TodoListView.swift
@@ -20,11 +20,29 @@
     
  */
 
+
+/*
+    Event 작업 목록
+    List
+    - CheckBox Image 변경 및 on/off 기능 추가
+    - ListRowView 클릭 시 새로운 View 띄우고 수정 및 저장 기능 추가
+    - Add 부분 NavigationBarItem 말고 아래 작은 + 원형 버튼으로 추가 (ZStack 사용)
+    
+    UI 부분 수정 및 추가할 부분
+    View
+    - MainView 인데 List만 보여주기가 좀..
+    - List 부분 위에 나의 목표 달성량 ? 아니면 전날 TodoList 달성량 보여주기
+    - Chart 부분 사용할 수 있으면 사용
+    - TabView 사용하여 화면 분리(Main Home, 캘린더)
+    - Add, Edit 부분 수정 후 MyPage Image 삽입 후 MyPageView 연결
+ */
+
 import SwiftUI
 
 struct TodoListView: View {
     
     // TodoList Items
+    /// Test용으로 문자열을 집어 넣음, 추 후 입력받은 데이터로 저장할 수 있도록 변경
     @State var items: [String] = [
         "first test",
         "second test",
@@ -32,6 +50,7 @@ struct TodoListView: View {
     ]
     
     var body: some View {
+        
         List {
             ForEach(items, id: \.self) { item in
                 ListRowView(title: item)
