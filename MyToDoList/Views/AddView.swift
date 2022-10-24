69fd4ba8e91864c134a757b4ceee8292a92dc73e
JJuKyungs <wnrud675@naver.com>
2022-10-24 17:26:45 +0900

    [Feat] AddView 수정 - ListViewModel 연결 - Button Action 구현 - TextField 안 Text 글자수 분별 Method 작성 - Alert 구현하여 TextField Text 글자수가 부족할 시 Alert 띄워줌

--- MyToDoList/Views/AddView.swift
+++ MyToDoList/Views/AddView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct AddView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    
+    @State var alerTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
@@ -17,12 +22,12 @@ struct AddView: View {
                 TextField("Type sometiong here...", text: $textFieldText)
                     .padding(.horizontal)
                     .frame(height: 55)
-                    .background(Color(.gray))
+                    .background(Color(.lightGray))
                     .cornerRadius(10)
+                    .opacity(0.7)
                 
-                Button {
-                    
-                } label: {
+                Button(action: willSaveButtonPressed,
+                       label: {
                     Text("Save".uppercased())
                         .foregroundColor(.white)
                         .font(.headline)
@@ -30,13 +35,44 @@ struct AddView: View {
                         .frame(maxWidth: .infinity)
                         .background(Color.accentColor)
                         .cornerRadius(10)
-                        
-                }
-
+                })
+                
             }
             .padding(15)
         }
         .navigationTitle("Add an Item")
+        .alert(isPresented: $showAlert) {
+            willGetAlert()
+        }
+    }
+    
+    // MARK: - willSaveButtonPressed()
+    func willSaveButtonPressed() {
+        
+        // TextField Count 가 3 이상일 때만 작동
+        if willTextIsAppropriate() {
+            listViewModel.didAddItem(title: textFieldText)
+            // presentationMode 를 이용하여 View 전 계층으로 돌아갈 수 있게 함
+            presentationMode.wrappedValue.dismiss()
+        }
+        
+    }
+    
+    // MARK: - willTextIsAppropriate
+    /// TextField 안의 Text의 글자수를 판별하여 Save Button을 활성화 할 수 있도록함
+    func willTextIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            // Alert 을 추가하여 보여줌
+            alerTitle = "입력 글자수를 3자를 넘겨주세요"
+            showAlert.toggle()
+            return false
+        }
+        
+        return true
+    }
+    
+    func willGetAlert() -> Alert {
+        return Alert(title: Text(alerTitle))
     }
 }
 
@@ -45,6 +81,6 @@ struct AddView_Previews: PreviewProvider {
         NavigationView {
             AddView()
         }
-        
+        .environmentObject(ListViewModel())
     }
 }
