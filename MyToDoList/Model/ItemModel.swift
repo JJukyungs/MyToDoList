37258b909d3bad5bebd86390c824c412f8db445a
JJuKyungs <wnrud675@naver.com>
2022-10-24 16:53:25 +0900

    [Feat] ItemModel 수정 - init() 을 사용하여 다시 호출하여 저장될 수 있도록 바꿔줌 - init을 활용하여 두가지 타입의 파라미터를 불러올수 있음 (id, title, isCompleted), (title, isCompleted) - id가 들어간 파라미터는 기존의 파라미터를 업데이트 할 시 사용, 없는 것은 item을 추가할 때 사용하면 됨 - willUpdateCompletion() Method 구현 :  Item 클릭 시 체크 이미지 변경을 위한 함수 (ItemModel을 새로 추가하여 변경해주는 것이 아닌 기존에 있던 Item을 다시 바꿔주기 위해 ItemModel에서 Method 구현)

--- MyToDoList/Model/ItemModel.swift
+++ MyToDoList/Model/ItemModel.swift
@@ -13,7 +13,20 @@ import Foundation
 
 struct ItemModel: Identifiable {
     // UUID()로 바로 입력할 수 도 있지만 String Type으로 반환하기 위해 .uuidString 을 사용
-    let id: String = UUID().uuidString
+    let id: String
     let title: String
     let isCompleted: Bool
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    // MARK: - willUpdateCompletion()
+    // - Item의 isCompleted 를 Update 하기 위해 사용하는 Method
+    // - ItemModel을 참조하도록 구조체에 선언
+    func willUpdateCompletion() -> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
 }
